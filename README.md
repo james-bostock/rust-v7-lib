982facd15c9cdb763c2cf85cf7fc90a5eb9c60b0
James Bostock <james.bostock@gmail.com>
2015-07-14 20:28:21 +1000

    Add implementation of the echo command.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rust-v6-cmd
 Some Sixth Edition Unix commands implemented in Rust.
+
+Commands implemented:
+
+* echo
---
+++ src/bin/echo.rs
@@ -0,0 +1,11 @@
+// Copyright 2015 James Bostock. See the LICENSE file at the top-level
+// directory of this distribution.
+
+// An implementation of the echo(1) command in Rust.
+// See http://man.cat-v.org/unix-6th/1/echo
+
+fn main() {
+  for arg in std::env::args().skip(1) {
+    println!("{}", arg);
+  }
+}
